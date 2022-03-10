a8b090c68e3459bc5cd0e6ed9b8b55d97c8f2cd3
lianxujie <2256996378@qq.com>
2022-03-10 19:54:28 +0800

    增加element-ui框架布局

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <!--
  * @Author: your name
  * @Date: 2022-03-10 19:09:35
- * @LastEditTime: 2022-03-10 19:24:01
+ * @LastEditTime: 2022-03-10 19:52:37
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \X北辰北\webgismap\webgismap\src\App.vue
 -->
 <template>
   <div id="app">
-    <p>23232</p>
+    <el-container class="app-out-pannel">
+      <el-header class="sys-header">Aside</el-header>
+      <el-container class="app-content-pannel">
+        <el-aside
+          width="200px"
+          class="sys-menu"
+        >左侧菜单</el-aside>
+        <el-main>地图区域</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -27,9 +36,44 @@ export default {
 html,
 body,
 #app {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  margin: 0;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+}
+.app-out-pannel,
+.app-content-pannel {
+    height: 100%;
+}
+.sys-header {
+    background-color: #32373e;
+    line-height: 60px;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 600;
+    display: flex;
+    justify-content: space-between;
+}
+.sys-menu {
+    background-color: #545c64;
+    width: 64px !important;
+    overflow: hidden !important;
+}
+.sys-content {
+    padding: 5px !important;
+}
+
+.morescreen-view {
+    width: 100%;
+    height: 820px;
+}
+.user-info {
+    font-size: 15px;
+}
+.user-info > span:last-child:hover {
+    color: #409eff;
+    cursor: pointer;
+}
+.login-content {
 }
 </style>
