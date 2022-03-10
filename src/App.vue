94f07a64141e32eec122cadded0bdf128618ce4c
lianxujie <2256996378@qq.com>
2022-03-10 19:25:11 +0800

    初始化项目结构

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-03-10 19:09:35
+ * @LastEditTime: 2022-03-10 19:24:01
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \X北辰北\webgismap\webgismap\src\App.vue
+-->
+<template>
+  <div id="app">
+    <p>23232</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+html,
+body,
+#app {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+</style>
