533cba358583be9732050f568b8c59483f8f1039
lianxujie <2256996378@qq.com>
2022-03-11 19:32:40 +0800

    优化首页布局

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-03-10 19:09:35
- * @LastEditTime: 2022-03-10 21:59:52
+ * @LastEditTime: 2022-03-11 19:30:15
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \X北辰北\webgismap\webgismap\src\App.vue
@@ -11,10 +11,27 @@
     <el-container class="app-out-pannel">
       <el-header class="sys-header">Aside</el-header>
       <el-container class="app-content-pannel">
-        <el-aside
-          width="200px"
-          class="sys-menu"
-        >左侧菜单</el-aside>
+        <el-aside class="sys-menu">
+          <el-menu
+            default-active="1"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            :collapse="true"
+          >
+            <el-menu-item index="1">
+              <i class="el-icon-monitor"></i>
+              <span slot="title">首页大屏</span>
+            </el-menu-item>
+            <el-menu-item index="2">
+              <i class="el-icon-picture-outline"></i>
+              <span slot="title">一张图</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
         <el-main>
           <MapView />
         </el-main>
@@ -30,8 +47,16 @@ export default {
   name: 'App',
   components: {
     MapView,
-  }
-}
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
 </script>
 
 <style>
